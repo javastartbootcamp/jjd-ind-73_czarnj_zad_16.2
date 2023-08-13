7a6d0447d75e7b80fc39e903d00d65c09d79684b
Julka Czarnasiak <czarna13500@gmail.com>
2023-08-13 11:54:36 +0200

    Add not working solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,12 @@
 package pl.javastart.task;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Main {
+    private static final DateTimeFormatter DISPLAYED_DATE_PATTERN = DatePattern.PATTERN_1.getFormatter();
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -10,7 +14,59 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // uzupełnij rozwiązanie. Korzystaj z przekazanego w parametrze scannera
+        System.out.println("Podaj datę:");
+        String date = scanner.nextLine();
+        DatePattern pattern = matchPattern(date);
+        if (pattern == null) {
+            System.out.println("Niepoprawna data");
+        } else {
+            String adjustedDate = adjustDate(date, pattern);
+            if (pattern.equals(DatePattern.PATTERN_2)) {
+                pattern = DatePattern.PATTERN_1;
+            }
+            LocalDateTime dateTime = LocalDateTime.parse(adjustedDate, pattern.getFormatter());
+            showTime(dateTime);
+        }
+    }
+
+    private void showTime(LocalDateTime dateTime) {
+        Instant instant = dateTime.toInstant(OffsetDateTime.now().getOffset());
+        System.out.printf("Czas lokalny: %s%n", getTimeForZone(instant, ZoneId.systemDefault()));
+        System.out.printf("UTC: %s%n", getTimeForZone(instant, ZoneId.of("UTC")));
+        System.out.printf("Londyn: %s%n", getTimeForZone(instant, ZoneId.of("Europe/London")));
+        System.out.printf("Los Angeles: %s%n", getTimeForZone(instant, ZoneId.of("America/Los_Angeles")));
+        System.out.printf("Sydney: %s%n", getTimeForZone(instant, ZoneId.of("Australia/Sydney")));
+    }
+
+    private String adjustDate(String date, DatePattern pattern) {
+        if (pattern.equals(DatePattern.PATTERN_2)) {
+            return date + " 00:00:00";
+        }
+        return date;
+    }
+
+    private String getTimeForZone(Instant instant, ZoneId zone) {
+        return ZonedDateTime.ofInstant(instant, zone).format(DISPLAYED_DATE_PATTERN);
+    }
+
+    private DatePattern matchPattern(String date) {
+        if (isDateValid(date, DatePattern.PATTERN_1.getFormatter())) {
+            return DatePattern.PATTERN_1;
+        } else if (isDateValid(date, DatePattern.PATTERN_2.getFormatter())) {
+            return DatePattern.PATTERN_2;
+        } else if (isDateValid(date, DatePattern.PATTERN_3.getFormatter())) {
+            return DatePattern.PATTERN_3;
+        }
+        return null;
+    }
+
+    private boolean isDateValid(String date, DateTimeFormatter pattern) {
+        try {
+            pattern.parse(date);
+            return true;
+        } catch (DateTimeParseException ex) {
+            return false;
+        }
     }
 
 }
